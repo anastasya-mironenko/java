bee90085155adbf2fc1fe644270850abccd6e687
Анастасия <ms.zloy@list.ru>
2018-03-15 22:30:34 +0300

    Реализация метода в классе Point

--- sandbox/src/main/java/MyFistProgramm.java
+++ sandbox/src/main/java/MyFistProgramm.java
@@ -3,20 +3,10 @@ import java.math.*;
 public class MyFistProgramm{
     public static void main (String[] args){
 
-        Point p1 = new Point();
-        p1.x = 1;
-        p1.y = 3;
+        Point p = new Point(1,3,4,3);
 
-        Point p2 = new Point();
-        p2.x = 3;
-        p2.y = 3;
-
-        System.out.println(distance(p1,p2));
+        System.out.println(p.distance());
 
     }
-    public static double distance(Point p1, Point p2){
 
-        return Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
-
-    }
 }
