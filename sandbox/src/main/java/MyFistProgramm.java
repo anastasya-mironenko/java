58f38c6d0ce5470d158ed5f4e9be4293fb1c48b9
Анастасия <ms.zloy@list.ru>
2018-03-15 21:51:53 +0300

    Создание функции по вычислению расстояния между точками

--- sandbox/src/main/java/MyFistProgramm.java
+++ sandbox/src/main/java/MyFistProgramm.java
@@ -1,5 +1,22 @@
+import java.math.*;
+
 public class MyFistProgramm{
-public static void main (String[] args){
-System.out.println("Hello, world");
-}
+    public static void main (String[] args){
+
+        Point p1 = new Point();
+        p1.x = 1;
+        p1.y = 3;
+
+        Point p2 = new Point();
+        p2.x = 3;
+        p2.y = 3;
+
+        System.out.println(distance(p1,p2));
+
+    }
+    public static double distance(Point p1, Point p2){
+
+        return Math.sqrt(Math.pow((p2.x-p1.x),2)+Math.pow((p2.y-p1.y),2));
+
+    }
 }
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,7 @@
+/**
+ * Created by Анастасия on 15.03.2018.
+ */
+public class Point {
+    public double x;
+    public double y;
+}
