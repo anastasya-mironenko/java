30113ffd51cc86c0c723f42eec2db1703b130960
Anastasia M <voronina_nastya9@mail,ru>
2018-03-20 16:24:44 +0300

    Улучшение конструкции if-else

--- sandbox/src/test/java/EquationTests.java
+++ sandbox/src/test/java/EquationTests.java
@@ -6,7 +6,7 @@ public class EquationTests {
     @Test
     public void test0() {
 
-        Equation e = new Equation(1,1,1);
+        Equation e = new Equation(1, 1, 1);
         Assert.assertEquals(e.rootNumder(), 0);
 
     }
@@ -14,7 +14,7 @@ public class EquationTests {
     @Test
     public void test1() {
 
-        Equation e = new Equation(1,2,1);
+        Equation e = new Equation(1, 2, 1);
         Assert.assertEquals(e.rootNumder(), 1);
 
     }
@@ -22,8 +22,29 @@ public class EquationTests {
     @Test
     public void test2() {
 
-        Equation e = new Equation(1,5,6);
+        Equation e = new Equation(1, 5, 6);
         Assert.assertEquals(e.rootNumder(), 2);
 
+    }
+
+    @Test
+    public void testLinear() {
+
+        Equation e = new Equation(0, 1, 1);
+        Assert.assertEquals(e.rootNumder(), 1);
+    }
+
+    @Test
+    public void testConstant() {
+
+        Equation e = new Equation(0, 0, 1);
+        Assert.assertEquals(e.rootNumder(), 0);
+    }
+
+    @Test
+    public void testZero() {
+
+        Equation e = new Equation(0, 0, 0);
+        Assert.assertEquals(e.rootNumder(), -1);
     }
 }
