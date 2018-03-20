94ec87738574391f4416bb6c335b0481eb635567
Anastasia M <voronina_nastya9@mail,ru>
2018-03-20 12:23:19 +0300

    Добавление модификации для контакта

---
+++ addressbook-web-tests/src/test/java/addressbook/tests/CustomerModificationTests.java
@@ -0,0 +1,18 @@
+package addressbook.tests;
+
+import addressbook.model.CustomerData;
+import org.testng.annotations.Test;
+
+public class CustomerModificationTests extends TestBase{
+
+    @Test
+    public void testCustomerModification(){
+
+        app.getNavigationHelper().gotoHomePage();
+        app.getContactHelper().selectContact();
+        app.getContactHelper().initContactModification();
+        app.getContactHelper().fillContactForm(new CustomerData("Testik11", "Testik2", "Testik3", "mobile1", "email1@mail.ru"));
+        app.getContactHelper().submitContactModification();
+        app.getNavigationHelper().returnHomePage();
+    }
+}
