b4092cd741a4314a83ae000f5430b659f18fdc88
Anastasia M <voronina_nastya9@mail,ru>
2018-03-20 11:36:18 +0300

    Добавление модификации группы

--- addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/addressbook/appmanager/ApplicationManager.java
@@ -18,7 +18,7 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
 
     public void init() {
-        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
+        wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files (x86)/Mozilla Firefox/firefox.exe"));
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost:8080/addressbook/group.php");
         groupHelper = new GroupHelper(wd);
